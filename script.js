4e628dfa3e180bfe2f73c83491682bb4e59f341f
Geovan <geovanc.genolos@gmail.com>
2024-02-09 09:39:04 +0800

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+const uniModal = document.getElementById('uniModal')
+const loaderHTML = `<div id="pre-loader"><div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div><div>`
+const flashdataHTML = document.createElement('div');
+       flashdataHTML.classList.add('flashdata')
+       flashdataHTML.classList.add('mb-3')
+       flashdataHTML.innerHTML = `<div class="d-flex w-100 align-items-center flex-wrap">
+           <div class="col-11 flashdata-msg"></div>
+           <div class="col-1 text-center">
+               <a href="javascript:void(0)" onclick="this.closest('.flashdata').remove()" class="flashdata-close"><i class="far fa-times-circle"></i></a>
+           </div>
+       </div>`;
