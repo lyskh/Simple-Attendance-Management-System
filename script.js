8edaf521bd16f7b89cc22fe28a53af20a310da00
Geovan <geovanc.genolos@gmail.com>
2024-02-10 14:27:08 +0800

    updated js

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ function start_loader(){
     $('body').prepend(loaderHTML)
     $('body, html').css('overflow', 'hidden')
 }
+function end_loader(){
+    if($('#pre-loader').length > 0)
+        $('#pre-loader').remove()
+    $('body, html').css('overflow', 'auto')
+}
+function open_modal($modalURL="", $modalTitle="", $data = {}, $size = "modal-md"){
+    if($modalURL == "")
+        return false;
+        $(uniModal).find('.modal-dialog').removeClass('modal-sm')
+        $(uniModal).find('.modal-dialog').removeClass('modal-md')
+        $(uniModal).find('.modal-dialog').removeClass('modal-lg')
+        $(uniModal).find('.modal-dialog').removeClass('modal-xl')
+        if($size != "")
+        $(uniModal).find('.modal-dialog').addClass($size)
+
+        $(uniModal).find('#uniModalLabel').html($modalTitle)
+
+        $(uniModal).find('.modal-body').html(`<div class="container-fluid"><div class="text-center text-dark-emphasis"><div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div></div></div>`)
+        $(uniModal).find('.modal-footer button[type="submit"]').hide()
+        $(uniModal).modal('show')
+        $.ajax({
+            url: `./modals/${$modalURL}`,
+            method: 'POST',
+            data: $data,
+            error: (err) => {
+                console.error(err)
+                alert("An error occurred while fetching the modal content! Kindly reload this page.")
+            },
+            success:function(resp){
+                $(uniModal).find('.modal-body').html(resp) 
+                var form = $(uniModal).find('.modal-body form')[0].getAttribute('id') || "";
+                if(form != ""){
+                    $(uniModal).find('.modal-footer button[type="submit"]').attr('form', form)
+                    $(uniModal).find('.modal-footer button[type="submit"]').show()
+                }
+            }
+        })
+
+}
+(function(){
+    $(uniModal).on('hidden.bs.modal', function(){
+        $(uniModal).find('.modal-body').html('')
+        $(uniModal).find('#uniModalLabel').html('')
+    })
+    $('body').on('scroll', function(){
+        console.log('test')
+    })
+})
