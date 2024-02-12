8dee17571fd547c99399dd40a63d335fb4b17d7b
Geovan Genolos <142429957+GeovanGenolos@users.noreply.github.com>
2024-02-12 22:26:00 +0800

    class form complete

--- modals/class_form.php
+++ modals/class_form.php
@@ -20,3 +20,45 @@ if(isset($_POST['id'])){
         </div>
     </form>
 </div>
+
+<script>
+  $('#class-form').submit(function(e){
+    e.preventDefault()
+    var _this = $(this)
+    start_loader();
+    $(uniModal).find('flashdata').remove()
+    var flashData = $('<div>')
+    flashData.addClass('flashdata mb-3')
+    flashData.html(`<div class="d-flex w-100 align-items-center flex-wrap">
+                      <div class="col-11 flashdata-msg"></div>
+                      <div class="col-1 text-center">
+                          <a href="javascript:void(0)" onclick="this.closest('.flashdata').remove()" class="flashdata-close"><i class="far fa-times-circle"></i></a>
+                      </div>
+              </div>`);
+    $.ajax({
+      url: "ajax-api.php?action=save_class",
+      method: "POST",
+      data: $(this).serialize(),
+      dataType:'JSON',
+      error: (err)=>{
+        flashData.find('.flashdata-msg').text(`An error occured!`)
+        flashData.addClass('flashdata-danger')
+        _this.prepend(flashData)
+        end_loader();
+        console.warn(err)
+      },
+      success: function(resp){
+        if(resp?.status == 'success'){
+          location.reload()
+        }else{
+          if(resp?.msg != ''){
+            flashData.find('.flashdata-msg').text(`${resp?.msg}`)
+            flashData.addClass('flashdata-danger')
+            _this.prepend(flashData)
+            end_loader();
+          }
+        }
+      }
+    })
+  })
+</script>
